980e9888f50003c9b68e6f2c9a3a504efa4ff92d
William Wu <williamwuu3@gmail.com>
2024-12-10 00:39:44 -0800

    added suspense to headerwrapper, adjusted some data.ts and completed 80% of service main

--- app/_component/servicepage/ServiceCard.tsx
+++ app/_component/servicepage/ServiceCard.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
 //used for display the service card in the service page main page
-const ServiceCard = ({ service }: { service: any }) => {
-	return <div>{service.title}</div>;
+const ServiceCard = ({ service, index }: { service: any, index: number }) => {
+	return (
+		<div className={`container mx-auto flex items-center ${index % 2 === 1 ? 'flex-row' : 'flex-row-reverse'}`}>
+			<div className="flex-1">
+				<h2>{service.title}</h2>
+				<p>{service?.fullDescription}</p>
+				<ul className="list-disc ml-6">
+					{service?.features?.map((feature: string, index: number) => (
+						<li key={index}>{feature}</li>
+					))}
+				</ul>
+				<Link 
+					href={`/service/${service.id}`}
+					className="inline-block mt-6 px-8 py-3 border border-white rounded-full hover:bg-white hover:text-black transition-all duration-300"
+				>
+					Learn More
+				</Link>
+			</div>
+			<div className="flex-1">
+				<Image src={service?.image} alt={service?.title} width={500} height={500} />
+			</div>
+		</div>
+	);
 };
 
 export default ServiceCard;
