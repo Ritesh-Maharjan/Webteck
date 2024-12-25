8eef586b39ad71868c6a9b69b3edc547ecea55f5
Ritesh <riteshmaharjan07@gmail.com>
2024-12-25 10:12:49 -0800

    Finished the offer scrolling

--- app/_component/Offer.tsx
+++ app/_component/Offer.tsx
@@ -1,12 +1,38 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef, useState } from "react";
 import HeadingTitle from "./ui/HeadingTitle";
 import Image from "next/image";
 import Card from "./ui/Card";
 
 const Offer = () => {
+  const sliderRef = useRef<HTMLDivElement>(null);
+  const [scrollPosition, setScrollPosition] = useState(0); // Current scroll position
+  const [maxScroll, setMaxScroll] = useState(10000); // MAx scroll position
+  let moveDirection;
+  const MOVE_DISTANCE = 1000;
+
+  const handleScroll = (direction: "left" | "right") => {
+    if (!sliderRef.current) return;
+
+    const container = sliderRef.current;
+    // How much width is visible in the screen
+    const containerWidth = container.offsetWidth;
+
+    // Subtracting total width with the width with visible width cause we dont want it to go overflow
+    const scrollWidth = container.scrollWidth - containerWidth;
+    setMaxScroll(scrollWidth);
+
+    if (direction == "right") {
+      moveDirection = Math.max(scrollPosition - MOVE_DISTANCE, -scrollWidth);
+    } else {
+      moveDirection = Math.min(scrollPosition + MOVE_DISTANCE, 0);
+    }
+    setScrollPosition(moveDirection);
+  };
+
   return (
-    <section className="pt-32 pb-72 bg-white text-black overflow-hidden">
-      <div className="max-w-screen-2xl px-8 mx-auto flex flex-col gap-4">
+    <section className="pt-32 pb-72 bg-white text-black">
+      <div className="max-w-screen-2xl px-8 mx-auto flex flex-col gap-4 overflow-hidden">
         <div className="flex justify-between items-end">
           <div className="flex flex-col gap-4">
             <HeadingTitle as="h2" backgroundColor="black" className="w-fit">
@@ -18,7 +44,11 @@ const Offer = () => {
             </p>
           </div>
           <div className="flex gap-4">
-            <button className="p-2 border-gray-400 border-2 rounded-full">
+            <button
+              disabled={scrollPosition === 0}
+              onClick={() => handleScroll("left")}
+              className="p-2 border-gray-400 border-2 rounded-full disabled:opacity-30"
+            >
               <Image
                 src="/img/home-page/whatweoffer-arrow-left.png"
                 height={24}
@@ -26,7 +56,11 @@ const Offer = () => {
                 alt="Left arrow"
               />
             </button>
-            <button className="p-2 border-gray-400 border-2 rounded-full">
+            <button
+              disabled={scrollPosition === -maxScroll}
+              onClick={() => handleScroll("right")}
+              className="p-2 border-gray-400 border-2 rounded-full disabled:opacity-30"
+            >
               <Image
                 src="/img/home-page/whatweoffer-arrow-right.png"
                 height={24}
@@ -37,102 +71,160 @@ const Offer = () => {
           </div>
         </div>
 
-        <div className="flex gap-2">
-          <Card cardBackground="radial-gradient(circle, #FFB5B5 15%, #EEEEEE 70%)">
-            <img src="/img/home-page/whatweoffer-1.png" className="w-14" alt="Website Development" />
-            <div>
-              <span className="text-sm font-bold">01.</span>
-              <h3 className="font-bold text-2xl">Website Development</h3>
-            </div>
-          </Card>
+        <div className="overflow-hidden">
+          <div
+            ref={sliderRef}
+            className="flex gap-2 relative transition-all duration-200"
+            style={{ left: `${scrollPosition}px` }}
+          >
+            <Card cardBackground="radial-gradient(circle, #FFB5B5 15%, #EEEEEE 70%)">
+              <img
+                src="/img/home-page/whatweoffer-1.png"
+                className="w-14"
+                alt="Website Development"
+              />
+              <div>
+                <span className="text-sm font-bold">01.</span>
+                <h3 className="font-bold text-2xl">Website Development</h3>
+              </div>
+            </Card>
 
-          <Card cardBackground="radial-gradient(circle, #B5E4FF 15%, #EEEEEE 70%)">
-            <img src="/img/home-page/whatweoffer-2.png" className="w-16" alt="SEO Service" />
-            <div>
-              <span className="text-sm font-bold">02.</span>
-              <h3 className="font-bold text-2xl">SEO Service</h3>
-            </div>
-          </Card>
+            <Card cardBackground="radial-gradient(circle, #B5E4FF 15%, #EEEEEE 70%)">
+              <img
+                src="/img/home-page/whatweoffer-2.png"
+                className="w-16"
+                alt="SEO Service"
+              />
+              <div>
+                <span className="text-sm font-bold">02.</span>
+                <h3 className="font-bold text-2xl">SEO Service</h3>
+              </div>
+            </Card>
 
-          <Card cardBackground="radial-gradient(circle, #FFE1B5 15%, #EEEEEE 70%)">
-            <img src="/img/home-page/whatweoffer-3.png" className="w-10" alt="Website Development" />
-            <div>
-              <span className="text-sm font-bold">03.</span>
-              <h3 className="font-bold text-2xl">Website Design</h3>
-            </div>
-          </Card>
+            <Card cardBackground="radial-gradient(circle, #FFE1B5 15%, #EEEEEE 70%)">
+              <img
+                src="/img/home-page/whatweoffer-3.png"
+                className="w-10"
+                alt="Website Development"
+              />
+              <div>
+                <span className="text-sm font-bold">03.</span>
+                <h3 className="font-bold text-2xl">Website Design</h3>
+              </div>
+            </Card>
 
-          <Card cardBackground="radial-gradient(circle, #BFB5FF 15%, #EEEEEE 70%)">
-            <img src="/img/home-page/whatweoffer-4.png" className="w-12" alt="Website Maintenance & Support" />
-            <div>
-              <span className="text-sm font-bold">04.</span>
-              <h3 className="font-bold text-2xl">Website Maintenance & Support</h3>
-            </div>
-          </Card>
+            <Card cardBackground="radial-gradient(circle, #BFB5FF 15%, #EEEEEE 70%)">
+              <img
+                src="/img/home-page/whatweoffer-4.png"
+                className="w-12"
+                alt="Website Maintenance & Support"
+              />
+              <div>
+                <span className="text-sm font-bold">04.</span>
+                <h3 className="font-bold text-2xl">
+                  Website Maintenance & Support
+                </h3>
+              </div>
+            </Card>
 
-          <Card cardBackground="radial-gradient(circle, #FFB5E1 15%, #EEEEEE 70%)">
-            <img src="/img/home-page/whatweoffer-5.png" className="w-12" alt="UI/UX Design" />
-            <div>
-              <span className="text-sm font-bold">05.</span>
-              <h3 className="font-bold text-2xl">UI/UX Design</h3>
-            </div>
-          </Card>
+            <Card cardBackground="radial-gradient(circle, #FFB5E1 15%, #EEEEEE 70%)">
+              <img
+                src="/img/home-page/whatweoffer-5.png"
+                className="w-12"
+                alt="UI/UX Design"
+              />
+              <div>
+                <span className="text-sm font-bold">05.</span>
+                <h3 className="font-bold text-2xl">UI/UX Design</h3>
+              </div>
+            </Card>
 
-          <Card cardBackground="radial-gradient(circle, #FFB5E1 15%, #EEEEEE 70%)">
-            <img src="/img/home-page/whatweoffer-6.png" className="w-12" alt="E-Commerce" />
-            <div>
-              <span className="text-sm font-bold">06.</span>
-              <h3 className="font-bold text-2xl">E-Commerce</h3>
-            </div>
-          </Card>
+            <Card cardBackground="radial-gradient(circle, #FFB5E1 15%, #EEEEEE 70%)">
+              <img
+                src="/img/home-page/whatweoffer-6.png"
+                className="w-12"
+                alt="E-Commerce"
+              />
+              <div>
+                <span className="text-sm font-bold">06.</span>
+                <h3 className="font-bold text-2xl">E-Commerce</h3>
+              </div>
+            </Card>
 
-          <Card cardBackground="radial-gradient(circle, #FFB5E1 15%, #EEEEEE 70%)">
-            <img src="/img/home-page/whatweoffer-7.png" className="w-12" alt="Website Design" />
-            <div>
-              <span className="text-sm font-bold">07.</span>
-              <h3 className="font-bold text-2xl">Website Design</h3>
-            </div>
-          </Card>
+            <Card cardBackground="radial-gradient(circle, #FFB5E1 15%, #EEEEEE 70%)">
+              <img
+                src="/img/home-page/whatweoffer-7.png"
+                className="w-12"
+                alt="Website Design"
+              />
+              <div>
+                <span className="text-sm font-bold">07.</span>
+                <h3 className="font-bold text-2xl">Website Design</h3>
+              </div>
+            </Card>
 
-          <Card cardBackground="radial-gradient(circle, #FFB5E1 15%, #EEEEEE 70%)">
-            <img src="/img/home-page/whatweoffer-8.png" className="w-12" alt="Branding and Graphic Design" />
-            <div>
-              <span className="text-sm font-bold">08.</span>
-              <h3 className="font-bold text-2xl">Branding and Graphic Design</h3>
-            </div>
-          </Card>
+            <Card cardBackground="radial-gradient(circle, #FFB5E1 15%, #EEEEEE 70%)">
+              <img
+                src="/img/home-page/whatweoffer-8.png"
+                className="w-12"
+                alt="Branding and Graphic Design"
+              />
+              <div>
+                <span className="text-sm font-bold">08.</span>
+                <h3 className="font-bold text-2xl">
+                  Branding and Graphic Design
+                </h3>
+              </div>
+            </Card>
 
-          <Card cardBackground="radial-gradient(circle, #FFB5E1 15%, #EEEEEE 70%)">
-            <img src="/img/home-page/whatweoffer-9.png" className="w-12" alt="Digital Marketing" />
-            <div>
-              <span className="text-sm font-bold">09.</span>
-              <h3 className="font-bold text-2xl">Digital Marketing</h3>
-            </div>
-          </Card>
+            <Card cardBackground="radial-gradient(circle, #FFB5E1 15%, #EEEEEE 70%)">
+              <img
+                src="/img/home-page/whatweoffer-9.png"
+                className="w-12"
+                alt="Digital Marketing"
+              />
+              <div>
+                <span className="text-sm font-bold">09.</span>
+                <h3 className="font-bold text-2xl">Digital Marketing</h3>
+              </div>
+            </Card>
 
-          <Card cardBackground="radial-gradient(circle, #FFB5E1 15%, #EEEEEE 70%)">
-            <img src="/img/home-page/whatweoffer-10.png" className="w-12" alt="Content Strategy" />
-            <div>
-              <span className="text-sm font-bold">10.</span>
-              <h3 className="font-bold text-2xl">Content Strategy</h3>
-            </div>
-          </Card>
+            <Card cardBackground="radial-gradient(circle, #FFB5E1 15%, #EEEEEE 70%)">
+              <img
+                src="/img/home-page/whatweoffer-10.png"
+                className="w-12"
+                alt="Content Strategy"
+              />
+              <div>
+                <span className="text-sm font-bold">10.</span>
+                <h3 className="font-bold text-2xl">Content Strategy</h3>
+              </div>
+            </Card>
 
-          <Card cardBackground="radial-gradient(circle, #FFB5E1 15%, #EEEEEE 70%)">
-            <img src="/img/home-page/whatweoffer-11.png" className="w-12" alt="Data & Analytics" />
-            <div>
-              <span className="text-sm font-bold">11.</span>
-              <h3 className="font-bold text-2xl">Data & Analytics</h3>
-            </div>
-          </Card>
+            <Card cardBackground="radial-gradient(circle, #FFB5E1 15%, #EEEEEE 70%)">
+              <img
+                src="/img/home-page/whatweoffer-11.png"
+                className="w-12"
+                alt="Data & Analytics"
+              />
+              <div>
+                <span className="text-sm font-bold">11.</span>
+                <h3 className="font-bold text-2xl">Data & Analytics</h3>
+              </div>
+            </Card>
 
-          <Card cardBackground="radial-gradient(circle, #FFB5E1 15%, #EEEEEE 70%)">
-            <img src="/img/home-page/whatweoffer-12.png" className="w-12" alt="Accessibility Compliance" />
-            <div>
-              <span className="text-sm font-bold">12.</span>
-              <h3 className="font-bold text-2xl">Accessibility Compliance</h3>
-            </div>
-          </Card>
+            <Card cardBackground="radial-gradient(circle, #FFB5E1 15%, #EEEEEE 70%)">
+              <img
+                src="/img/home-page/whatweoffer-12.png"
+                className="w-12"
+                alt="Accessibility Compliance"
+              />
+              <div>
+                <span className="text-sm font-bold">12.</span>
+                <h3 className="font-bold text-2xl">Accessibility Compliance</h3>
+              </div>
+            </Card>
+          </div>
         </div>
       </div>
     </section>
