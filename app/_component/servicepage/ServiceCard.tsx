33dc71ccd01a801e20b3dae2696ee4d16598b5f1
Ritesh <riteshmaharjan07@gmail.com>
2024-12-28 11:51:50 -0800

    Fixed a design a bit and removed env variables

--- app/_component/servicepage/ServiceCard.tsx
+++ app/_component/servicepage/ServiceCard.tsx
@@ -3,30 +3,39 @@ import Image from "next/image";
 import Link from "next/link";
 
 //used for display the service card in the service page main page
-const ServiceCard = ({ service, index }: { service: any, index: number }) => {
-	return (
-		<div className={`container mx-auto flex items-center ${index % 2 === 1 ? 'flex-row' : 'flex-row-reverse'}`}>
-			<div className="flex-1">
-				<h2>{service.title}</h2>
-				<p>THIS IS THE SERVICE CARD</p>
-				<p>{service?.fullDescription}</p>
-				<ul className="list-disc ml-6">
-					{service?.features?.map((feature: string, index: number) => (
-						<li key={index}>{feature}</li>
-					))}
-				</ul>
-				<Link 
-					href={`/service/${service.id}`}
-					className="inline-block mt-6 px-8 py-3 border border-white rounded-full hover:bg-white hover:text-black transition-all duration-300"
-				>
-					Learn More
-				</Link>
-			</div>
-			<div className="flex-1">
-				<Image src={service?.image} alt={service?.title} width={500} height={500} />
-			</div>
-		</div>
-	);
+const ServiceCard = ({ service, index }: { service: any; index: number }) => {
+  return (
+    <div
+      className={`container flex-col mx-auto flex justify-center gap-10 md:items-center ${
+        index % 2 === 1 ? "md:flex-row" : "md:flex-row-reverse"
+      }`}
+    >
+      <div className="max-w-[650px]">
+        <h2>{service.title}</h2>
+        <p>THIS IS THE SERVICE CARD</p>
+        <p>{service?.fullDescription}</p>
+        <ul className="list-disc ml-6">
+          {service?.features?.map((feature: string, index: number) => (
+            <li key={index}>{feature}</li>
+          ))}
+        </ul>
+        <Link
+          href={`/service/${service.id}`}
+          className="inline-block mt-6 px-8 py-3 border border-white rounded-full hover:bg-white hover:text-black transition-all duration-300"
+        >
+          Learn More
+        </Link>
+      </div>
+      <div className="w-[450px] shrink-0">
+        <Image
+          src={service?.image}
+          alt={service?.title}
+          width={450}
+          height={500}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ServiceCard;
--- app/service/page.tsx
+++ app/service/page.tsx
@@ -1,49 +1,57 @@
-import { servicesData } from './data';
-import ServiceCard from '@/app/_component/servicepage/ServiceCard';
-import Image from 'next/image';
-import Link from 'next/link';
-import TextSlider, { defaultTexts } from '@/app/_component/ui/TextSlider';
-import Investment from '../_component/Investment';
+import { servicesData } from "./data";
+import ServiceCard from "@/app/_component/servicepage/ServiceCard";
+import Image from "next/image";
+import Link from "next/link";
+import TextSlider, { defaultTexts } from "@/app/_component/ui/TextSlider";
+import Investment from "../_component/Investment";
+import HeadingTitle from "../_component/ui/HeadingTitle";
 
 export default function ServicePage() {
-	return (
-		//THIS IS THE SERVICE PAGE MAIN
-		<main className='w-full'>
-			<section className='hero-section relative h-screen w-full z-0'>
-				<Image
-					src='/img/service-page/service-hero-bg.png'
-					alt='Hero Image'
-					fill
-					priority //might need to optimize the image for performance so i can remove this WARNING**
-					className='object-cover absolute top-0 left-0 w-full h-full z-0'
-				/>
-				<div className='flex flex-col justify-between h-full'>
-					<div className='flex flex-row items-center justify-center h-full max-w-[1300px] mx-auto px-4'>
-						<div className='flex flex-col gap-4 text-center'>
-							<h3 className='text-xl font-bold text-left relative z-10'>Services</h3>
-							<h1 className='text-6xl font-bold text-left relative z-10'>We cultivate trust alongside building websites.</h1>
-						</div>
-						<div className='mt-4'>
-							<h3 className='text-xl font-bold text-left relative z-10'>Our extensive knowledge in design and technology brings forth leading brands and digital journeys.</h3>
-						</div>
-					</div>
-					<div className='relative z-10'>
-						<TextSlider texts={defaultTexts} />
-					</div>
-				</div>
-			</section>
-			<section className='gradient-borders mx-auto'>
-				<div className='flex flex-col gap-4'>
-					{servicesData.map((service) => (
-						<ServiceCard 
-							key={service.id} 
-							service={service} 
-							index={servicesData.indexOf(service)} 
-						/>
-					))}
-				</div>
-				<Investment />
-			</section>
-		</main>
-	);
+  return (
+    //THIS IS THE SERVICE PAGE MAIN
+    <main className="w-full">
+      <section className="hero-section relative w-full z-0">
+        <Image
+          src="/img/service-page/service-hero-bg.png"
+          alt="Hero Image"
+          fill
+          priority //might need to optimize the image for performance so i can remove this WARNING**
+          className="object-cover absolute top-0 left-0 w-full h-full z-0"
+        />
+        <div className="flex flex-col justify-between h-full">
+          <div className="flex flex-col md:flex-row md:items-center justify-center min-h-[calc(100vh-3rem-155px)] max-w-screen-2xl mx-auto px-4">
+            <div className="flex flex-col gap-4 text-center max-w-2xl">
+              <h3 className="text-xl font-bold text-left relative z-10 text-[#D9D9D9]">
+                Services
+              </h3>
+              <HeadingTitle as="h1" className="text-left relative">
+                We cultivate trust alongside building websites.
+              </HeadingTitle>
+            </div>
+            <div className="mt-4 w-[418px]">
+              <p className="text-2xl text-[#DBDBDB] text-left relative z-10">
+                Our extensive knowledge in design and technology brings forth
+                leading brands and digital journeys.
+              </p>
+            </div>
+          </div>
+          <div className="relative z-10">
+            <TextSlider texts={defaultTexts} />
+          </div>
+        </div>
+      </section>
+      <section className="gradient-borders mt-4 mx-auto max-w-screen-2xl">
+        <div className="flex flex-col gap-4">
+          {servicesData.map((service) => (
+            <ServiceCard
+              key={service.id}
+              service={service}
+              index={servicesData.indexOf(service)}
+            />
+          ))}
+        </div>
+        <Investment />
+      </section>
+    </main>
+  );
 }
