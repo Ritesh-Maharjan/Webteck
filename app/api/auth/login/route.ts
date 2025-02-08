a9c459d4dd394b3d6c4c0620354756e973c4aaf9
Ritesh <riteshmaharjan07@gmail.com>
2025-02-08 15:02:28 -0800

    Adding the correct callback url

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,8 +1,11 @@
 import { NextResponse } from "next/server";
 
-export function GET() {
+export function GET(req: Request) {
+  const referer = req.headers.get("referer") || req.headers.get("origin");
+  console.log(referer);
+
   const githubAuthURL = `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}&scope=repo,user&redirect_uri=${encodeURIComponent(
-    "http://localhost:3000/api/auth/callback"
+    "https://webteck.ca/api/auth/callback"
   )}`;
   return NextResponse.redirect(githubAuthURL);
 }
