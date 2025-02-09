58ac62d840bc8c79755c53de2215c60d9b8f0795
Ritesh <riteshmaharjan07@gmail.com>
2025-02-09 00:00:09 -0800

    Modified the route

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,8 +1,6 @@
 import { NextResponse } from "next/server";
 
-export function GET(req: Request) {
-  const referer = req.headers.get("referer") || req.headers.get("origin");
-  console.log(referer);
+export function GET() {
 
   const githubAuthURL = `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}&scope=repo,user&redirect_uri=${encodeURIComponent(
     "https://webteck.ca/api/auth/callback"
