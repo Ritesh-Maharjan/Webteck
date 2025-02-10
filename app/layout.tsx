bb85d70afda5f452406f4e889ba910ab3a9595aa
Baagii <baagii1993@yahoo.com>
2025-02-10 15:57:03 -0800

    trying to get back favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({
     <html lang="en" dir="ltr">
       <head>
         {/* Favicon and other icons */}
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/public/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
         <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
